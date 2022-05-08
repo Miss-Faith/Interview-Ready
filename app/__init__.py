fa7c63041a61a7b9607723ba4de1cb4ddc50f8bb
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-08 19:52:18 +0300

    update config file and create app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,11 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
     #Initializing Flask Extensions
     login_manager.init_app(app)
