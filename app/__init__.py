f5c3d13370be022139502ba5de397d3a557050f6
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-08 20:38:56 +0300

    Update dependencies and create email file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,10 +27,6 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    # setting config
-    from .request import configure_request
-    configure_request(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
