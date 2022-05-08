7eceaad215ea355dfe41b55b5f612499497a3bc9
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-08 19:03:01 +0300

    Install flask login-in, initialize flask login and import log in required

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
 
+    #Initializing Flask Extensions
+    login_manager.init_app(app)
+
+    
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
