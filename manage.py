4ba8f00c4b83c4a7b21b8eaa86c76c14492a34eb
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-08 20:02:26 +0300

    update manage file

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 from app import create_app,db
+from app.models import User,Role,Comment
+from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
+
+migrate = Migrate(app,db)
 
 if __name__ == '__main__':
     app.run()
