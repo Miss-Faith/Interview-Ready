5dd28c1c713362a6ba59ce74cebf051d27dca04c
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-10 02:25:49 +0300

    update config file

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     General configuration parent class
     '''
    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI ='postgres://imrfzlraxpkqnm:ac435e7b9684829d5e89c89bee1a98124e36efc61cdabdaa3e391d84262b9c8e@ec2-3-229-11-55.compute-1.amazonaws.com:5432/df0ci4803u7nda'
+    SQLALCHEMY_DATABASE_URI ='postgresql://imrfzlraxpkqnm:ac435e7b9684829d5e89c89bee1a98124e36efc61cdabdaa3e391d84262b9c8e@ec2-3-229-11-55.compute-1.amazonaws.com:5432/df0ci4803u7nda'
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     UPLOADED_PHOTOS_DEST ='app/static/photos'
@@ -28,7 +28,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgres://imrfzlraxpkqnm:ac435e7b9684829d5e89c89bee1a98124e36efc61cdabdaa3e391d84262b9c8e@ec2-3-229-11-55.compute-1.amazonaws.com:5432/df0ci4803u7nda'
+    SQLALCHEMY_DATABASE_URI ='postgresql://imrfzlraxpkqnm:ac435e7b9684829d5e89c89bee1a98124e36efc61cdabdaa3e391d84262b9c8e@ec2-3-229-11-55.compute-1.amazonaws.com:5432/df0ci4803u7nda'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
     #     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
