b8d027845b9350f55329940656bfbc6e03f14214
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-09 13:33:47 +0300

    Update categories and views

--- app/main/views.py
+++ app/main/views.py
@@ -35,7 +35,7 @@ def new_pitch():
         category = form.category.data
         user_id = current_user
         new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
-        new_pitch_object.save_p()
+        new_pitch_object.save_pitch()
         return redirect(url_for('main.categories'))
         
     return render_template('create_pitch.html', form = form)
